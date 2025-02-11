9e1e4ca880ee7731526f38d3975545dd26c0f9aa
PinkyThomasIG <pinkymukalal@gmail.com>
2025-02-11 06:27:54 +0100

    updated the validation for the new user

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const dotenv = require("dotenv");
-const { body, validationResult } = require("express-validator");
+const { check, validationResult } = require("express-validator");
 
 //const { check, validationResult } = require("express-validator");
 const express = require("express"); // To handle HTTP requests and responses
@@ -34,22 +34,24 @@ mongoose
 app.post(
   "/users",
   [
-    body("Username").not().isEmpty().withMessage("Username is required"),
-    body("Password").not().isEmpty().withMessage("Password is required"),
-    body("Email").isEmail().withMessage("Invalid email format"),
+    check("username", "Username is required").isLength({ min: 5 }),
+    check(
+      "username",
+      "Username contains non alphanumeric characters - not allowed"
+    ).isAlphanumeric(),
+    check("password", "password is required").not().isEmpty(),
+    check("email", "Email does not appear to be valid").isEmail(),
   ],
   async (req, res) => {
-    // Check for validation errors
-    const errors = validationResult(req);
+    let errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(422).json({ errors: array() });
     }
-
     let hashedPassword = Users.hashPassword(req.body.Password);
-
-    await Users.findOne({ Username: req.body.Username })
+    await Users.findOne({ Username: req.body.Username }) // Search to see if a user with the requested username already exists
       .then((user) => {
         if (user) {
+          //If the user is found, send a response that it already exists
           return res.status(400).send(req.body.Username + " already exists");
         } else {
           Users.create({
@@ -73,35 +75,6 @@ app.post(
       });
   }
 );
-
-/* app.post("/users", async (req, res) => {
-  let hashedPassword = Users.hashPassword(req.body.Password);
-  await Users.findOne({ Username: req.body.Username }) // Search to see if a user with the requested username already exists
-    .then((user) => {
-      if (user) {
-        //If the user is found, send a response that it already exists
-        return res.status(400).send(req.body.Username + " already exists");
-      } else {
-        Users.create({
-          Username: req.body.Username,
-          Password: hashedPassword,
-          Email: req.body.Email,
-          Birthday: req.body.Birthday,
-        })
-          .then((user) => {
-            res.status(201).json(user);
-          })
-          .catch((error) => {
-            console.error(error);
-            res.status(500).send("Error: " + error);
-          });
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).send("Error: " + error);
-    });
-}); */
 
 // Update user info by username
 app.put(
@@ -232,7 +205,7 @@ app.delete(
 
 // READ
 // Get all movies
-app.get(
+/* app.get(
   "/movies",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
@@ -244,6 +217,23 @@ app.get(
         console.error(error);
         res.status(500).send("Error: " + error);
       });
+  }
+); */
+
+app.get(
+  "/movies",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    try {
+      const movies = await Movies.find(); // Fetch all movies from MongoDB
+      if (!movies.length) {
+        return res.status(404).send("No movies found");
+      }
+      res.status(200).json(movies);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send("Error fetching movies");
+    }
   }
 );
 
@@ -303,7 +293,7 @@ app.get(
   (req, res) => {
     // Use the correct genre name field in the query: "Genre.name"
     Movies.find({
-      "Genre.name": { $regex: new RegExp(req.params.genreName, "i") },
+      "Genre.name": { $regex: new RegExp(req.params.genreName, "i") }, // Case-insensitive search
     })
       .then((movies) => {
         if (movies.length > 0) {
